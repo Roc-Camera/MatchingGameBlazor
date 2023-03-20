b7367fe1510ad953eb7e09ef7e537a677515ae8e
Roc <1608976833@qq.com>
2023-03-20 14:15:29 +0800

    提交文件

---
+++ BlazorMatchGame/wwwroot/js/script.js
@@ -0,0 +1,17 @@
+﻿function ScriptAlert(message) {
+    alert(message);
+}
+
+  function HelloWorld(){
+    console.log('hello world');
+}
+ function displayRandom(randomIdName){
+    console.log(randomIdName);
+    var my = document.getElementById(randomIdName);
+    my.style.visibility = "visible";
+};
+function closePreRandom(id)
+{
+    var you = document.getElementById(id)
+    you.style.visibility = "hidden";
+}
